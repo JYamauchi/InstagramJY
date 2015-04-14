e4bd9d634e79e9bb3114af5d1b4b256154c6b5c9
Jared Yamauchi <jared.yamauchi@gmail.com>
2015-04-14 07:46:11 -0400

    W05D01

---
+++ wdinstagram_app/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Entry.create(author: "HD-Wallpapers", photo_url: "http://www.hdwallpapers.in/walls/waikiki_at_dusk_hawaii-normal.jpg")
+Entry.create(author: "Designgrapher", photo_url: "http://designgrapher.com/wp-content/uploads/2013/06/nature-photographs.jpg")
+Entry.create(author: "HongKiat", photo_url: "http://www.betterphoto.com/uploads/processed/0724/0706140402101moore_5-2.jpg")
+Entry.create(author: "InspireFirst", photo_url: "http://www.inspirefirst.com/wp-content/uploads/2014/02/001-nature-photography-oerwout.jpg")
+Entry.create(author: "WebNeel", photo_url: "http://webneel.com/daily/sites/default/files/images/daily/10-2013/3-nature-photography-cherry-tree.jpg")
+Entry.create(author: "SmashingPicture", photo_url: "http://smashingpicture.com/wp-content/uploads/2011/03/alex11.jpg")
